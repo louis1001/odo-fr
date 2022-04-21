0bedfa8f5ab76510aefd82a03793ae467ac16d64
louis1001 <louis1001dev@gmail.com>
2022-04-20 18:21:41 -0600

    Moves project to swift package

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version: 5.6
+import PackageDescription
+
+let package = Package(
+    name: "odo",
+    dependencies: [
+        .package(url: "https://github.com/andybest/linenoise-swift", from: "0.0.3"),
+    ],
+    targets: [
+        .target(name: "odolib", dependencies: [], path: "Sources/odo/"),
+        .target(name: "odo_repl", dependencies: [
+            .product(name: "LineNoise", package: "linenoise-swift"),
+            "odolib"
+        ])
+    ])
